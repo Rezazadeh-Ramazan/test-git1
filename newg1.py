77fc2a33b55dbe2eeb6171a9e14968c3f13f9321
Ramzi_Rezazadeh <sora.razayan@gmail.com>
2024-04-26 16:41:03 -0700

    added ther hights

--- newg1.py
+++ newg1.py
@@ -3,8 +3,13 @@ name1 = 'rozi'
 name2 = 'hadi'
 name3 = 'hani'
 name4 = 'hama'
-# step 2 : add ther ages:
+# step 3 : add ther ages:
 ag1 = 4
 ag2 = 11
 ag3 = 8
 ag4 = 6
+# step4 adding hights 
+hig1 = 110
+hig2 = 130
+hig3 = 122
+hig4 = 114
